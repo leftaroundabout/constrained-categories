898f5ea9e9a6b29a411d24833fdd03101b8671e3
Justus Sagemüller <sagemuej@smail.uni-koeln.de>
2012-11-25 15:44:13 +0100

    Added infix synonyms, analogue to the ones from Data.Functor

--- Control/Functor/Constrained.hs
+++ Control/Functor/Constrained.hs
@@ -20,11 +20,22 @@ module Control.Functor.Constrained where
 
 import GHC.Exts
 
+infixl 4  <$#
+
 class CFunctor f where
   type CFunctorCtxt f a :: Constraint
 --   type CFunctorCtxt f a = ()
   cfmap :: (CFunctorCtxt f a, CFunctorCtxt f b)
      => (a -> b) -> f a -> f b
 
+  (<$#) :: (CFunctorCtxt f a, CFunctorCtxt f b)
+     => a -> f b -> f a
+  (<$#) = cfmap . const
 -- instance (Functor f) => CFunctor f where
 --   cfmap = fmap
+
+infixl 4 <$>#
+
+(<$>#) :: (CFunctorCtxt f a, CFunctorCtxt f b)
+     => (a -> b) -> f a -> f b
+(<$>#) = cfmap
