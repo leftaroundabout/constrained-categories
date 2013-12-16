b291885b4a7c443179987f19f9dc1d6c6e554014
Justus Sagemüller <sagemuej@smail.uni-koeln.de>
2013-12-16 15:28:54 +0100

    Some fixes in 'Monad'. Doesn't quite work yet, but very nearly.

--- Control/Monad/Constrained.hs
+++ Control/Monad/Constrained.hs
@@ -20,19 +20,22 @@ import qualified Prelude
 
 class (Applicative m k k) => Monad m k where
   return :: (Object k a, Object k (m a)) => k a (m a)
-  join :: (Object k (m a), Object k (m (m a)))
+  join :: (Object k a, Object k (m a), Object k (m (m a)))
        => m (m a) `k` m a
 
          
 
-infixl 1 >>=
-(=<<) :: (Monad m k, Object k a, Object k (m a), Object k (m b), Object k (m (m b)))
+infixr 1 =<<
+(=<<) :: ( Monad m k, Object k a, Object k b
+         , Object k (m a), Object k (m b), Object k (m (m b)) )
       => k a (m b) -> k (m a) (m b)
 (=<<) q = join . fmap q
 
-infixr 1 =<<
-(>>=) :: (Monad m (->)) => m a -> (a -> m b) -> m b
-(>>=) = flip (=<<)
+infixl 1 >>=
+(>>=) :: ( Function f, Monad m f, Object f a, Object f b
+         , Object f (m a), Object f (m b), Object f (m (m b)) ) 
+             => m a -> f a (m b) -> m b
+g >>= h = (=<<) h $ g
 
 instance Monad ((->)a) (->) where
   return = const
