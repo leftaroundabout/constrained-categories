37d4cd1a2dac092b40b8f17ba1023c17676293c0
Justus Sagemüller <sagemuej@smail.uni-koeln.de>
2013-12-17 16:38:38 +0100

    Added simple wrapper module to qualifiedly re-export the Prelude / Hask versions of Functor etc.. Edited .cabal file.

---
+++ Control/Category/Hask.hs
@@ -0,0 +1,4 @@
+module Control.Category.Hask(module Prelude, module Control.Category, module Control.Applicative) where
+import Prelude hiding ((.), id)
+import Control.Category
+import Control.Applicative
