931a5136492445d3ef53626882fd59c1877e72d6
Justus Sagemüller <sagemuej@smail.uni-koeln.de>
2013-12-17 16:25:48 +0100

    Replaced single particular instances of Functors '(a->)' and '[]' with generic instance for _all_ Hask-functors from the Prelude version of that class.
    Unfortunately, such a generic instance appears to make any other instances that are not monomorphic in their category impossible (functional-dependency conflict). That's unfortunate, but at least 'Forgetful' appeared to be not all that useful, so we removed it.

--- Control/Functor/Constrained.hs
+++ Control/Functor/Constrained.hs
@@ -8,7 +8,6 @@
 module Control.Functor.Constrained ( module Control.Category.Constrained
                                    , Functor(..)
                                    , constrainedFmap
-                                   , Forgetful(..)
                                    ) where
 
 
@@ -22,11 +21,8 @@ class (Category r, Category t) => Functor f r t | f r -> t, f t -> r where
   fmap :: (Object r a, Object r (f a), Object t b, Object t (f b))
      => r a b -> t (f a) (f b)
 
-instance Functor ((->)a) (->) (->) where
-  fmap = (.)
-
-instance Functor [] (->) (->) where
-  fmap = map
+instance (Prelude.Functor f) => Functor f (->) (->) where
+  fmap = Prelude.fmap
 
   
 constrainedFmap :: (Category r, Category t, o a, o b, o (f a), o (f b)) 
@@ -34,12 +30,6 @@ constrainedFmap :: (Category r, Category t, o a, o b, o (f a), o (f b))
        -> ConstrainedCategory r o a b -> ConstrainedCategory t o (f a) (f b)
 constrainedFmap q = constrained . q . unconstrained
 
-
-newtype Forgetful a = Forgetful { runForgetful :: a }
-   deriving (Eq, Ord, Show, Read)
-
-instance Functor Forgetful (ConstrainedCategory (->) o) (->) where
-  fmap q = Forgetful . unconstrained q . runForgetful
 
   
 
