ea7d43651cbdea950a06df335ee012c8b41b6129
Justus Sagemüller <sagemuej@smail.uni-koeln.de>
2014-01-22 02:38:43 +0100

    Foldable and Traversable actually to work with non-Hask categories.
    The constraints of 'mapM_' are rather crazy right now, but seem to work...

--- Data/Foldable/Constrained.hs
+++ Data/Foldable/Constrained.hs
@@ -47,12 +47,15 @@ import Control.Arrow.Constrained
 -- forM_ (x:xs) f = (f $ x) >> forM_ xs f
 -- 
 class (Functor t k l) => Foldable t k l where
+  ffoldl :: ( Object k a, Object k b, PairObject k a b
+            , Object l a, Object l (t b), PairObject l a (t b)
+            ) => k (a,b) a -> l (a,t b) a
   foldMap :: ( Object k a, Object l (t a), Monoid m, Object k m, Object l m )
                => (a `k` m) -> t a `l` m
-  mapM_ :: ( Monoidal f l l, Monoidal f k k, Monoid u, UnitObject k u
-           , Object k a, Object k (f u), Object l (t a), Object l (f u) )
-           => a `k` f u -> t a `l` f u
-
+--  mapM_ :: ( Monoidal f l l, Monoidal f k k, Monoid (UnitObject k)
+--           , Object k a, Object l (t a)
+--           ) => a `k` f (UnitObject k) -> t a `l` f (UnitObject l)
+--
 fold :: (Foldable t k k, Monoid m, Object k m, Object k (t m)) => t m `k` m
 fold = foldMap id
 
@@ -61,30 +64,73 @@ instance (Category k, Object k a) => Monoid (Endo' k a) where
   mempty = Endo' id
   mappend (Endo' f) (Endo' g) = Endo' $ f . g
 
--- newtype Monoidal_ (r :: * -> * -> *) (s :: * -> * -> *) (f :: * -> *) (u :: *) 
---       = Monoidal { runMonoidal :: f u }
--- instance ( Monoidal f k k, Function k, Object k u, UnitObject k u 
---          , PairObject k u u, Monoid u )
---                   => Monoid (Monoidal_ k f u) where
---   mempty = memptyMdl 
---   mappend (Monoidal x) (Monoidal y) = Monoidal (fzipWith detachUnit $ (x, y))
--- 
--- memptyMdl :: forall k f u . ( Monoidal f k k, Function k, Object k u, UnitObject k u 
---                             , PairObject k u u, Monoid u )
--- memptyMdl = Monoidal (pure $ mempty)
--- 
+newtype Monoidal_ (r :: * -> * -> *) (s :: * -> * -> *) (f :: * -> *) (u :: *) 
+      = Monoidal { runMonoidal :: f u }
+instance ( Monoidal f k k, Function k
+         , u ~ UnitObject k, Monoid u 
+         , PairObject k u u, PairObject k (f u) (f u), Object k (f u,f u)
+         ) => Monoid (Monoidal_ k k f u) where
+  mempty = memptyMdl
+  mappend = mappendMdl
+
+memptyMdl :: forall r s f u v . ( Monoidal f r s, Function s
+                                , PairObject s u u, Monoid v
+                                , u~UnitObject r, v~UnitObject s )
+               => Monoidal_ r s f u
+memptyMdl = Monoidal ((pureUnit :: s v (f u)) $ mempty)
+mappendMdl :: forall r s f u v . ( Monoidal f r s, Function s
+                                , PairObject r u u, PairObject s (f u) (f u)
+                                , Object s (f u, f u), Monoid v
+                                , u~UnitObject r, v~UnitObject s )
+               => Monoidal_ r s f u -> Monoidal_ r s f u -> Monoidal_ r s f u
+mappendMdl (Monoidal x) (Monoidal y) 
+      = Monoidal (combine $ (x, y))
+ where combine :: s (f u, f u) (f u)
+       combine = fzipWith detachUnit
+
 
 
 instance Foldable [] (->) (->) where
-  foldMap f [] = mempty
+  foldMap _ [] = mempty
   foldMap f (x:xs) = f x <> foldMap f xs
-  mapM_ _ [] = pure mempty
-  mapM_ f (x:xs) = fzipWith (uncurry mappend) (f x, mapM_ f xs)
+  -- mapM_ _ [] = pureUnit mempty
+  -- mapM_ f (x:xs) = fzipWith detachUnit (f x, mapM_ f xs)
+  ffoldl f = uncurry $ foldl (curry f)
 
 instance Foldable Maybe (->) (->) where
   foldMap f Nothing = mempty
   foldMap f (Just x) = f x
-  mapM_ _ Nothing = pure mempty
-  mapM_ f (Just x) = f x
+  -- mapM_ _ Nothing = pureUnit mempty
+  -- mapM_ f (Just x) = f x
+  ffoldl _ (i,Nothing) = i
+  ffoldl f (i,Just a) = f(i,a)
 
 
+instance ( Foldable f s t, Arrow s (->), Arrow t (->)
+         , Functor f (ConstrainedCategory s o) (ConstrainedCategory t o) 
+         -- , UnitObject (ConstrainedCategory t o) ~ UnitObject s 
+         ) => Foldable f (ConstrainedCategory s o) (ConstrainedCategory t o) where
+  foldMap (ConstrainedMorphism f) = ConstrainedMorphism $ foldMap f
+  ffoldl (ConstrainedMorphism f) = ConstrainedMorphism $ ffoldl f
+--  mapM_ = mapM_Cs 
+--
+mapM_ :: forall t k l o f a uk ul .
+           ( Foldable t k l, Arrow k (->), Arrow l (->)
+           , Functor t k l, Monoidal f l l, Monoidal f k k
+           , Object k a, Object l (t a)
+           , PairObject l (f ul) (t a), PairObject k (f ul) a
+           , Object l (f ul, t a), Object l (ul, t a), Object l (t a, ul)
+           , PairObject l ul (t a), PairObject l (t a) ul
+           , PairObject k (f ul) (f ul), PairObject k ul ul
+           , Object k (f ul, f ul), Object k (f ul, a)
+           , uk ~ UnitObject k, ul ~ UnitObject l, uk ~ ul
+           ) => a `k` f uk -> t a `l` f ul
+mapM_ f = ffoldl q . first pureUnit . swap . attachUnit
+    where q :: k (f uk, a) (f uk)
+          q = fzipWith detachUnit . second f
+--   -- mapM_ f = arr runMonoidal . foldMap (arr Monoidal . f)
+--   mapM_ (ConstrainedMorphism f) = arr mM_
+--    where mM_ [] = pure `inCategoryOf` f $ mempty
+--          mM_ (x:xs) = fzipWith (arr $ uncurry mappend) `inCategoryOf` f 
+--                                                 $ (f $ x, mM_ xs)
+-- 
