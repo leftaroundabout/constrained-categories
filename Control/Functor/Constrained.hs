de8d1991f85ca08919c7508915f324994adde78f
Justus Sagemüller <sagemuej@smail.uni-koeln.de>
2014-06-14 14:31:28 +0200

    Introduced 'SumToProduct' class.

--- Control/Functor/Constrained.hs
+++ Control/Functor/Constrained.hs
@@ -1,23 +1,28 @@
 -- |
 -- Module      :  Control.Functor.Constrained
--- Copyright   :  (c) 2013 Justus Sagemüller
+-- Copyright   :  (c) 2014 Justus Sagemüller
 -- License     :  GPL v3 (see COPYING)
 -- Maintainer  :  (@) sagemuej $ smail.uni-koeln.de
 -- 
 
 {-# LANGUAGE ConstraintKinds              #-}
 {-# LANGUAGE TypeFamilies                 #-}
+{-# LANGUAGE TypeOperators                #-}
 {-# LANGUAGE FunctionalDependencies       #-}
 {-# LANGUAGE FlexibleInstances            #-}
 {-# LANGUAGE FlexibleContexts             #-}
 {-# LANGUAGE UndecidableInstances         #-}
 
 
-module Control.Functor.Constrained ( module Control.Category.Constrained
-                                   , Functor(..)
-                                   , (<$>)
-                                   , constrainedFmap
-                                   ) where
+module Control.Functor.Constrained
+   ( module Control.Category.Constrained
+     -- * Functors
+   , Functor(..)
+   , (<$>)
+   , constrainedFmap
+     -- * [Co]product mapping
+   , SumToProduct(..)
+   ) where
 
 
 import Control.Category.Constrained
@@ -25,6 +30,7 @@ import Control.Category.Constrained
 import Prelude hiding (id, (.), Functor(..))
 import qualified Prelude
 
+import Data.Void
 
 class ( Category r, Category t, Object t (f (UnitObject r)) )
            => Functor f r t | f r -> t, f t -> r where
@@ -33,6 +39,33 @@ class ( Category r, Category t, Object t (f (UnitObject r)) )
 
 instance (Prelude.Functor f) => Functor f (->) (->) where
   fmap = Prelude.fmap
+
+-- | It is fairly common for (typically, container-like) functors to map 'Either'
+--   to tuples in a natural way, thus \"separating the variants\".
+--   This is related to 'Data.Foldable.Constrained.Foldable', but rather simpler.
+class ( CoCartesian r, Cartesian t, Functor f r t, Object t (f (ZeroObject r)) )
+           => SumToProduct f r t where
+  -- | @
+  --   sum2product ≡ mapEither id
+  --   @
+  sum2product :: ( ObjectSum r a b, ObjectPair t (f a) (f b) )
+       => t (f (a+b)) (f a, f b)
+  -- | @
+  --   mapEither f ≡ sum2product . fmap f
+  --   @
+  mapEither :: ( Object r a, ObjectSum r b c
+               , Object t (f a), ObjectPair t (f b) (f c) )
+       => r a (b+c) -> t (f a) (f b, f c)
+
+instance SumToProduct [] (->) (->) where
+  sum2product [] = ([],[])
+  sum2product (Left x  : l) = (x:xs, ys) where ~(xs,ys) = sum2product l
+  sum2product (Right y : l) = (xs ,y:ys) where ~(xs,ys) = sum2product l
+  mapEither _ [] = ([],[])
+  mapEither f (a:l) = case f a of
+      Left x  -> (x:xs, ys)
+      Right y -> (xs ,y:ys)
+   where ~(xs,ys) = mapEither f l
 
 (<$>) :: (Functor f r (->), Object r a, Object r b)
      => r a b -> f a -> f b
@@ -48,6 +81,10 @@ instance (Functor [] k k, o [UnitObject k])
        => Functor [] (ConstrainedCategory k o) (ConstrainedCategory k o) where
   fmap (ConstrainedMorphism f) = ConstrainedMorphism $ fmap f
 
+instance (o (), o [()], o Void, o [Void]) => SumToProduct []
+     (ConstrainedCategory (->) o) (ConstrainedCategory (->) o) where
+  sum2product = ConstrainedMorphism sum2product
+  mapEither (ConstrainedMorphism f) = ConstrainedMorphism $ mapEither f
 
   
 
