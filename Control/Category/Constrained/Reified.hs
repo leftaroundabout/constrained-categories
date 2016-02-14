09f57c68914b1e1f28b729502171274175663b52
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2016-02-14 17:13:14 +0100

    Move polymorphic pattern synomyms for reified-category deconstruction to new module.

--- Control/Category/Constrained/Reified.hs
+++ Control/Category/Constrained/Reified.hs
@@ -26,29 +26,14 @@
 {-# LANGUAGE UnicodeSyntax         #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE CPP                   #-}
-{-# LANGUAGE PatternSynonyms       #-}
-{-# LANGUAGE ViewPatterns          #-}
 
 module Control.Category.Constrained.Reified (
       -- * Reified versions of the category classes
-         ReCategory
-       , ReCartesian
-       , ReMorphism
-       , RePreArrow
-       , ReWellPointed
-      -- * Pattern synonyms
-      -- ** Category
-       , pattern Specific, pattern Id, pattern (:<<<), pattern (:>>>)
-      -- ** Cartesian
-       , pattern Swap
-       , pattern AttachUnit, pattern DetachUnit
-       , pattern Regroup, pattern Regroup'
-      -- ** Morphism
-       , pattern (:***)
-      -- ** Pre-arrow
-       , pattern (:&&&), pattern Fst, pattern Snd, pattern Terminal
-      -- ** Well-pointed
-       , pattern Const
+         ReCategory(..)
+       , ReCartesian(..)
+       , ReMorphism(..)
+       , RePreArrow(..)
+       , ReWellPointed(..)
        ) where
 
 
@@ -81,7 +66,6 @@ data UnitObjWitness k u where
 
 
 
-infixr 1 :>>>, :<<<
 
 -- GHC-invoked CPP can't seem able to do token pasting, so invoke the
 -- preprocessor manually to generate the GADTs.
@@ -112,40 +96,11 @@ data ReCategory (k :: * -> * -> *) (α :: *) (β :: *) where
 
 instance Category k => Category (ReCategory k) where
   type Object (ReCategory k) α = Object k α
-  id = Id
-  CATEGORYCOMPO
+  id = CategoryId
+  CategoryId . f = f
+  g . CategoryId = g
   g . f = CategoryCompo f g
 
-data IdPattern k α β where
-    IsId :: Object k α => IdPattern k α α
-    NotId :: IdPattern k α β
-data CompoPattern k α β where
-    IsCompo :: Object k β
-         => k α β -> k β γ -> CompoPattern k α γ
-    NotCompo :: CompoPattern k α β
-class Category k => CRCategory k where
-  type SpecificCat k :: * -> * -> *
-  fromSpecific :: SpecificCat k α β -> k α β
-  match_concrete :: k α β -> Maybe (SpecificCat k α β)
-  match_id :: k α β -> IdPattern k α β
-  match_compose :: k α β -> CompoPattern k α β
-
-instance Category k => CRCategory (ReCategory k) where
-  type SpecificCat (ReCategory k) = k
-  fromSpecific = ReCategory
-  match_concrete (ReCategory f) = Just f
-  match_concrete _ = Nothing
-  match_id CategoryId = IsId
-  match_id _ = NotId
-  match_compose (CategoryCompo f g) = IsCompo f g
-  match_compose _ = NotCompo
-
-pattern Specific f <- (match_concrete -> Just f) where
-  Specific f = fromSpecific f
-pattern Id <- (match_id -> IsId) where
-  Id = id
-pattern g:<<<f <- (match_compose -> IsCompo f g)
-pattern f:>>>g <- (match_compose -> IsCompo f g)
   
 instance HasAgent k => HasAgent (ReCategory k) where
   type AgentVal (ReCategory k) α ω = GenericAgent (ReCategory k) α ω
@@ -188,7 +143,11 @@ data ReCartesian (k :: * -> * -> *) (α :: *) (β :: *) where
 instance Cartesian k => Category (ReCartesian k) where
   type Object (ReCartesian k) a = Object k a
   id = CartesianId
-  CARTESIANCOMPO
+  CartesianSwap . CartesianSwap = id
+  CartesianRegroup . CartesianRegroup_ = id
+  CartesianRegroup_ . CartesianRegroup = id
+  CartesianId . f = f
+  g . CartesianId = g
   g . f = CartesianCompo f g
 
 instance Cartesian k => Cartesian (ReCartesian k) where
@@ -200,62 +159,6 @@ instance Cartesian k => Cartesian (ReCartesian k) where
   regroup = CartesianRegroup
   regroup' = CartesianRegroup_
   
-instance Cartesian k => CRCategory (ReCartesian k) where
-  type SpecificCat (ReCartesian k) = k
-  fromSpecific = ReCartesian
-  match_concrete (ReCartesian f) = Just f
-  match_concrete _ = Nothing
-  match_id (CartesianId) = IsId
-  match_id _ = NotId
-  match_compose (CartesianCompo f g) = IsCompo f g
-  match_compose _ = NotCompo
-
-data SwapPattern k α β where
-    IsSwap :: (ObjectPair k α β, ObjectPair k β α)
-                 => SwapPattern k (α,β) (β,α)
-    NotSwap :: SwapPattern k α β
-data AttachUnitPattern k α β where
-    IsAttachUnit :: (Object k α, UnitObject k ~ u, ObjectPair k α u)
-                 => AttachUnitPattern k α (α,u)
-    NotAttachUnit :: AttachUnitPattern k α β
-data DetachUnitPattern k α β where
-    IsDetachUnit :: (Object k α, UnitObject k ~ u, ObjectPair k α u)
-                 => DetachUnitPattern k (α,u) α
-    NotDetachUnit :: DetachUnitPattern k α β
-data RegroupPattern k α β where
-    IsRegroup :: ( ObjectPair k α β, ObjectPair k β γ
-                 , ObjectPair k α (β,γ), ObjectPair k (α,β) γ )
-                 => RegroupPattern k (α,(β,γ)) ((α,β),γ)
-    NotRegroup :: RegroupPattern k α β
-data Regroup'Pattern k α β where
-    IsRegroup' :: ( ObjectPair k α β, ObjectPair k β γ
-                 , ObjectPair k α (β,γ), ObjectPair k (α,β) γ )
-                 => Regroup'Pattern k ((α,β),γ) (α,(β,γ))
-    NotRegroup' :: Regroup'Pattern k α β
-class CRCategory k => CRCartesian k where
-  match_swap :: k α β -> SwapPattern k α β
-  match_attachUnit :: k α β -> AttachUnitPattern k α β
-  match_detachUnit :: k α β -> DetachUnitPattern k α β
-  match_regroup :: k α β -> RegroupPattern k α β
-  match_regroup' :: k α β -> Regroup'Pattern k α β
-
-instance Cartesian k => CRCartesian (ReCartesian k) where
-  match_swap (CartesianSwap) = IsSwap
-  match_swap _ = NotSwap
-  match_attachUnit (CartesianAttachUnit) = IsAttachUnit
-  match_attachUnit _ = NotAttachUnit
-  match_detachUnit (CartesianDetachUnit) = IsDetachUnit
-  match_detachUnit _ = NotDetachUnit
-  match_regroup (CartesianRegroup) = IsRegroup
-  match_regroup _ = NotRegroup
-  match_regroup' (CartesianRegroup_) = IsRegroup'
-  match_regroup' _ = NotRegroup'
-
-pattern Swap <- (match_swap -> IsSwap)
-pattern AttachUnit <- (match_attachUnit -> IsAttachUnit)
-pattern DetachUnit <- (match_detachUnit -> IsDetachUnit)
-pattern Regroup <- (match_regroup -> IsRegroup) 
-pattern Regroup' <- (match_regroup' -> IsRegroup')
   
 instance (HasAgent k, Cartesian k) => HasAgent (ReCartesian k) where
   type AgentVal (ReCartesian k) α ω = GenericAgent (ReCartesian k) α ω
@@ -265,8 +168,6 @@ instance (HasAgent k, Cartesian k) => HasAgent (ReCartesian k) where
   
 instance Cartesian k => EnhancedCat (ReCartesian k) k where arr = ReCartesian
 
-
-infixr 3 :***
 
 #ifdef GADTCPP
 #  define REMORPHISM(C)                                \
@@ -287,7 +188,12 @@ data ReMorphism (k :: * -> * -> *) (α :: *) (β :: *) where
 instance Morphism k => Category (ReMorphism k) where
   type Object (ReMorphism k) a = Object k a
   id = MorphismId
-  MORPHISMCOMPO
+  (f`MorphismPar`g) . (h`MorphismPar`i) = f.h *** g.i
+  MorphismSwap . MorphismSwap = id
+  MorphismRegroup . MorphismRegroup_ = id
+  MorphismRegroup_ . MorphismRegroup = id
+  MorphismId . f = f
+  g . MorphismId = g
   g . f = MorphismCompo f g
 
 instance Morphism k => Cartesian (ReMorphism k) where
@@ -307,46 +213,10 @@ instance (HasAgent k, Morphism k) => HasAgent (ReMorphism k) where
   alg = genericAlg
   ($~) = genericAgentMap
 
-instance Morphism k => CRCategory (ReMorphism k) where
-  type SpecificCat (ReMorphism k) = k
-  fromSpecific = ReMorphism
-  match_concrete (ReMorphism f) = Just f
-  match_concrete _ = Nothing
-  match_id (MorphismId) = IsId
-  match_id _ = NotId
-  match_compose (MorphismCompo f g) = IsCompo f g
-  match_compose _ = NotCompo
-
-instance Morphism k => CRCartesian (ReMorphism k) where
-  match_swap (MorphismSwap) = IsSwap
-  match_swap _ = NotSwap
-  match_attachUnit (MorphismAttachUnit) = IsAttachUnit
-  match_attachUnit _ = NotAttachUnit
-  match_detachUnit (MorphismDetachUnit) = IsDetachUnit
-  match_detachUnit _ = NotDetachUnit
-  match_regroup (MorphismRegroup) = IsRegroup
-  match_regroup _ = NotRegroup
-  match_regroup' (MorphismRegroup_) = IsRegroup'
-  match_regroup' _ = NotRegroup'
-
-data ParPattern k α β where
-    IsPar :: (ObjectPair k α γ, ObjectPair k β δ)
-         => k α β -> k γ δ -> ParPattern k (α,γ) (β,δ)
-    NotPar :: ParPattern k α β
-class CRCartesian k => CRMorphism k where
-  match_par :: k α β -> ParPattern k α β
-
-instance Morphism k => CRMorphism (ReMorphism k) where
-  match_par (MorphismPar f g) = IsPar f g
-  match_par _ = NotPar
-
-pattern f:***g <- (match_par -> IsPar f g)
   
 instance Morphism k => EnhancedCat (ReMorphism k) k where arr = ReMorphism
 
 
-
-infixr 3 :&&&
 
 
 #ifdef GADTCPP
@@ -375,7 +245,17 @@ data RePreArrow (k :: * -> * -> *) (α :: *) (β :: *) where
 instance PreArrow k => Category (RePreArrow k) where
   type Object (RePreArrow k) a = Object k a
   id = PreArrowId
-  PREARROWCOMPO
+  PreArrowTerminal . _ = terminal
+  PreArrowFst . (f`PreArrowFanout`_) = f
+  PreArrowSnd . (_`PreArrowFanout`g) = g
+  PreArrowFst . (f`PreArrowPar`_) = f . fst
+  PreArrowSnd . (_`PreArrowPar`g) = g . snd
+  (f`PreArrowPar`g) . (h`PreArrowPar`i) = f.h *** g.i
+  PreArrowSwap . PreArrowSwap = id
+  PreArrowRegroup . PreArrowRegroup_ = id
+  PreArrowRegroup_ . PreArrowRegroup = id
+  PreArrowId . f = f
+  g . PreArrowId = g
   g . f = PreArrowCompo f g
 
 instance PreArrow k => Cartesian (RePreArrow k) where
@@ -401,68 +281,6 @@ instance (HasAgent k, PreArrow k) => HasAgent (RePreArrow k) where
   alg = genericAlg
   ($~) = genericAgentMap
 
-instance PreArrow k => CRCategory (RePreArrow k) where
-  type SpecificCat (RePreArrow k) = k
-  fromSpecific = RePreArrow
-  match_concrete (RePreArrow f) = Just f
-  match_concrete _ = Nothing
-  match_id (PreArrowId) = IsId
-  match_id _ = NotId
-  match_compose (PreArrowCompo f g) = IsCompo f g
-  match_compose _ = NotCompo
-
-instance PreArrow k => CRCartesian (RePreArrow k) where
-  match_swap (PreArrowSwap) = IsSwap
-  match_swap _ = NotSwap
-  match_attachUnit (PreArrowAttachUnit) = IsAttachUnit
-  match_attachUnit _ = NotAttachUnit
-  match_detachUnit (PreArrowDetachUnit) = IsDetachUnit
-  match_detachUnit _ = NotDetachUnit
-  match_regroup (PreArrowRegroup) = IsRegroup
-  match_regroup _ = NotRegroup
-  match_regroup' (PreArrowRegroup_) = IsRegroup'
-  match_regroup' _ = NotRegroup'
-
-instance PreArrow k => CRMorphism (RePreArrow k) where
-  match_par (PreArrowPar f g) = IsPar f g
-  match_par _ = NotPar
-
-data FanPattern k α β where
-    IsFan :: (Object k α, ObjectPair k β γ)
-         => k α β -> k α γ -> FanPattern k α (β,γ)
-    NotFan :: FanPattern k α β
-data FstPattern k α β where
-    IsFst :: (ObjectPair k α β)
-                 => FstPattern k (α,β) α
-    NotFst :: FstPattern k α β
-data SndPattern k α β where
-    IsSnd :: (ObjectPair k α β)
-                 => SndPattern k (α,β) β
-    NotSnd :: SndPattern k α β
-data TerminalPattern k α β where
-    IsTerminal :: (Object k α, UnitObject k ~ u)
-                 => TerminalPattern k α u
-    NotTerminal :: TerminalPattern k α β
-class CRCartesian k => CRPreArrow k where
-  match_fan :: k α β -> FanPattern k α β
-  match_fst :: k α β -> FstPattern k α β
-  match_snd :: k α β -> SndPattern k α β
-  match_terminal :: k α β -> TerminalPattern k α β
-
-pattern f:&&&g <- (match_fan -> IsFan f g)
-pattern Fst <- (match_fst -> IsFst)
-pattern Snd <- (match_snd -> IsSnd)
-pattern Terminal <- (match_terminal -> IsTerminal)
-  
-instance PreArrow k => CRPreArrow (RePreArrow k) where
-  match_fan (PreArrowFanout f g) = IsFan f g
-  match_fan _ = NotFan
-  match_fst PreArrowFst = IsFst
-  match_fst _ = NotFst
-  match_snd PreArrowSnd = IsSnd
-  match_snd _ = NotSnd
-  match_terminal PreArrowTerminal = IsTerminal
-  match_terminal _ = NotTerminal
 
 instance PreArrow k => EnhancedCat (RePreArrow k) k where arr = RePreArrow
 
@@ -488,7 +306,18 @@ data ReWellPointed (k :: * -> * -> *) (α :: *) (β :: *) where
 instance WellPointed k => Category (ReWellPointed k) where
   type Object (ReWellPointed k) a = Object k a
   id = WellPointedId
-  PREARROWCOMPO
+  WellPointedConst c . _ = const c
+  WellPointedTerminal . _ = terminal
+  WellPointedFst . (f`WellPointedFanout`_) = f
+  WellPointedSnd . (_`WellPointedFanout`g) = g
+  WellPointedFst . (f`WellPointedPar`_) = f . fst
+  WellPointedSnd . (_`WellPointedPar`g) = g . snd
+  (f`WellPointedPar`g) . (h`WellPointedPar`i) = f.h *** g.i
+  WellPointedSwap . WellPointedSwap = id
+  WellPointedRegroup . WellPointedRegroup_ = id
+  WellPointedRegroup_ . WellPointedRegroup = id
+  WellPointedId . f = f
+  g . WellPointedId = g
   g . f = WellPointedCompo f g
 
 instance WellPointed k => Cartesian (ReWellPointed k) where
@@ -530,54 +359,6 @@ instance (HasAgent k, WellPointed k) => HasAgent (ReWellPointed k) where
   alg = genericAlg
   ($~) = genericAgentMap
 
-instance WellPointed k => CRCategory (ReWellPointed k) where
-  type SpecificCat (ReWellPointed k) = k
-  fromSpecific = ReWellPointed
-  match_concrete (ReWellPointed f) = Just f
-  match_concrete _ = Nothing
-  match_id (WellPointedId) = IsId
-  match_id _ = NotId
-  match_compose (WellPointedCompo f g) = IsCompo f g
-  match_compose _ = NotCompo
-
-instance WellPointed k => CRCartesian (ReWellPointed k) where
-  match_swap (WellPointedSwap) = IsSwap
-  match_swap _ = NotSwap
-  match_attachUnit (WellPointedAttachUnit) = IsAttachUnit
-  match_attachUnit _ = NotAttachUnit
-  match_detachUnit (WellPointedDetachUnit) = IsDetachUnit
-  match_detachUnit _ = NotDetachUnit
-  match_regroup (WellPointedRegroup) = IsRegroup
-  match_regroup _ = NotRegroup
-  match_regroup' (WellPointedRegroup_) = IsRegroup'
-  match_regroup' _ = NotRegroup'
-
-instance WellPointed k => CRMorphism (ReWellPointed k) where
-  match_par (WellPointedPar f g) = IsPar f g
-  match_par _ = NotPar
-  
-instance WellPointed k => CRPreArrow (ReWellPointed k) where
-  match_fan (WellPointedFanout f g) = IsFan f g
-  match_fan _ = NotFan
-  match_fst WellPointedFst = IsFst
-  match_fst _ = NotFst
-  match_snd WellPointedSnd = IsSnd
-  match_snd _ = NotSnd
-  match_terminal WellPointedTerminal = IsTerminal
-  match_terminal _ = NotTerminal
-
-data ConstPattern k α β where
-    IsConst :: (Object k α, Object k β)
-                 => β -> ConstPattern k α β
-    NotConst :: ConstPattern k α β
-class CRPreArrow k => CRWellPointed k where
-  match_const :: k α β -> ConstPattern k α β
-
-pattern Const c <- (match_const -> IsConst c)
-  
-instance WellPointed k => CRWellPointed (ReWellPointed k) where
-  match_const (WellPointedConst c) = IsConst c
-  match_const _ = NotConst
 
 instance WellPointed k => EnhancedCat (ReWellPointed k) k where arr = ReWellPointed
 
---
+++ Control/Category/Constrained/Reified/PolyPattern.hs
@@ -0,0 +1,296 @@
+-- |
+-- Module      :  Control.Category.Constrained.Reified.PolyPattern
+-- Copyright   :  (c) 2016 Justus Sagemüller
+-- License     :  GPL v3 (see COPYING)
+-- Maintainer  :  (@) sagemueller $ geo.uni-koeln.de
+-- 
+-- 
+-- Pattern synonyms which allow you to deconstruct the various types
+-- in "Control.Category.Constrained.Reified" in a uniform way.
+-- 
+-- This kind of polymorphic pattern (with @ViewPatterns@) doesn't
+-- seem to work prior to GHC-7.10, so if you have base<4.8 this
+-- module will be empty.
+
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ConstraintKinds       #-}
+{-# LANGUAGE UnicodeSyntax         #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE CPP                   #-}
+{-# LANGUAGE PatternSynonyms       #-}
+{-# LANGUAGE ViewPatterns          #-}
+
+module Control.Category.Constrained.Reified.PolyPattern (
+      -- * Pattern synonyms
+      -- ** Category
+         pattern Specific, pattern Id, pattern (:<<<), pattern (:>>>)
+      -- ** Cartesian
+       , pattern Swap
+       , pattern AttachUnit, pattern DetachUnit
+       , pattern Regroup, pattern Regroup'
+      -- ** Morphism
+       , pattern (:***)
+      -- ** Pre-arrow
+       , pattern (:&&&), pattern Fst, pattern Snd, pattern Terminal
+      -- ** Well-pointed
+       , pattern Const
+      -- * Deconstruction-classes
+       , CRCategory(..), CRCartesian(..), CRMorphism(..), CRPreArrow(..), CRWellPointed(..)
+       ) where
+
+
+import Prelude ()
+import GHC.Exts (Constraint)
+
+import Control.Category.Constrained.Prelude
+import Control.Arrow.Constrained
+import Control.Category.Constrained.Reified
+
+import Data.Tagged
+
+
+data IdPattern k α β where
+    IsId :: Object k α => IdPattern k α α
+    NotId :: IdPattern k α β
+data CompoPattern k α β where
+    IsCompo :: Object k β
+         => k α β -> k β γ -> CompoPattern k α γ
+    NotCompo :: CompoPattern k α β
+class Category k => CRCategory k where
+  type SpecificCat k :: * -> * -> *
+  fromSpecific :: SpecificCat k α β -> k α β
+  match_concrete :: k α β -> Maybe (SpecificCat k α β)
+  match_id :: k α β -> IdPattern k α β
+  match_compose :: k α β -> CompoPattern k α β
+
+instance Category k => CRCategory (ReCategory k) where
+  type SpecificCat (ReCategory k) = k
+  fromSpecific = ReCategory
+  match_concrete (ReCategory f) = Just f
+  match_concrete _ = Nothing
+  match_id CategoryId = IsId
+  match_id _ = NotId
+  match_compose (CategoryCompo f g) = IsCompo f g
+  match_compose _ = NotCompo
+
+pattern Specific f <- (match_concrete -> Just f) where
+  Specific f = fromSpecific f
+pattern Id <- (match_id -> IsId) where
+  Id = id
+pattern g:<<<f <- (match_compose -> IsCompo f g)
+pattern f:>>>g <- (match_compose -> IsCompo f g)
+  
+instance Cartesian k => CRCategory (ReCartesian k) where
+  type SpecificCat (ReCartesian k) = k
+  fromSpecific = ReCartesian
+  match_concrete (ReCartesian f) = Just f
+  match_concrete _ = Nothing
+  match_id (CartesianId) = IsId
+  match_id _ = NotId
+  match_compose (CartesianCompo f g) = IsCompo f g
+  match_compose _ = NotCompo
+
+data SwapPattern k α β where
+    IsSwap :: (ObjectPair k α β, ObjectPair k β α)
+                 => SwapPattern k (α,β) (β,α)
+    NotSwap :: SwapPattern k α β
+data AttachUnitPattern k α β where
+    IsAttachUnit :: (Object k α, UnitObject k ~ u, ObjectPair k α u)
+                 => AttachUnitPattern k α (α,u)
+    NotAttachUnit :: AttachUnitPattern k α β
+data DetachUnitPattern k α β where
+    IsDetachUnit :: (Object k α, UnitObject k ~ u, ObjectPair k α u)
+                 => DetachUnitPattern k (α,u) α
+    NotDetachUnit :: DetachUnitPattern k α β
+data RegroupPattern k α β where
+    IsRegroup :: ( ObjectPair k α β, ObjectPair k β γ
+                 , ObjectPair k α (β,γ), ObjectPair k (α,β) γ )
+                 => RegroupPattern k (α,(β,γ)) ((α,β),γ)
+    NotRegroup :: RegroupPattern k α β
+data Regroup'Pattern k α β where
+    IsRegroup' :: ( ObjectPair k α β, ObjectPair k β γ
+                 , ObjectPair k α (β,γ), ObjectPair k (α,β) γ )
+                 => Regroup'Pattern k ((α,β),γ) (α,(β,γ))
+    NotRegroup' :: Regroup'Pattern k α β
+class CRCategory k => CRCartesian k where
+  match_swap :: k α β -> SwapPattern k α β
+  match_attachUnit :: k α β -> AttachUnitPattern k α β
+  match_detachUnit :: k α β -> DetachUnitPattern k α β
+  match_regroup :: k α β -> RegroupPattern k α β
+  match_regroup' :: k α β -> Regroup'Pattern k α β
+
+instance Cartesian k => CRCartesian (ReCartesian k) where
+  match_swap (CartesianSwap) = IsSwap
+  match_swap _ = NotSwap
+  match_attachUnit (CartesianAttachUnit) = IsAttachUnit
+  match_attachUnit _ = NotAttachUnit
+  match_detachUnit (CartesianDetachUnit) = IsDetachUnit
+  match_detachUnit _ = NotDetachUnit
+  match_regroup (CartesianRegroup) = IsRegroup
+  match_regroup _ = NotRegroup
+  match_regroup' (CartesianRegroup_) = IsRegroup'
+  match_regroup' _ = NotRegroup'
+
+pattern Swap <- (match_swap -> IsSwap)
+pattern AttachUnit <- (match_attachUnit -> IsAttachUnit)
+pattern DetachUnit <- (match_detachUnit -> IsDetachUnit)
+pattern Regroup <- (match_regroup -> IsRegroup) 
+pattern Regroup' <- (match_regroup' -> IsRegroup')
+  
+infixr 3 :***
+
+instance Morphism k => CRCategory (ReMorphism k) where
+  type SpecificCat (ReMorphism k) = k
+  fromSpecific = ReMorphism
+  match_concrete (ReMorphism f) = Just f
+  match_concrete _ = Nothing
+  match_id (MorphismId) = IsId
+  match_id _ = NotId
+  match_compose (MorphismCompo f g) = IsCompo f g
+  match_compose _ = NotCompo
+
+instance Morphism k => CRCartesian (ReMorphism k) where
+  match_swap (MorphismSwap) = IsSwap
+  match_swap _ = NotSwap
+  match_attachUnit (MorphismAttachUnit) = IsAttachUnit
+  match_attachUnit _ = NotAttachUnit
+  match_detachUnit (MorphismDetachUnit) = IsDetachUnit
+  match_detachUnit _ = NotDetachUnit
+  match_regroup (MorphismRegroup) = IsRegroup
+  match_regroup _ = NotRegroup
+  match_regroup' (MorphismRegroup_) = IsRegroup'
+  match_regroup' _ = NotRegroup'
+
+data ParPattern k α β where
+    IsPar :: (ObjectPair k α γ, ObjectPair k β δ)
+         => k α β -> k γ δ -> ParPattern k (α,γ) (β,δ)
+    NotPar :: ParPattern k α β
+class CRCartesian k => CRMorphism k where
+  match_par :: k α β -> ParPattern k α β
+
+instance Morphism k => CRMorphism (ReMorphism k) where
+  match_par (MorphismPar f g) = IsPar f g
+  match_par _ = NotPar
+
+pattern f:***g <- (match_par -> IsPar f g)
+  
+
+infixr 3 :&&&
+
+
+instance PreArrow k => CRCategory (RePreArrow k) where
+  type SpecificCat (RePreArrow k) = k
+  fromSpecific = RePreArrow
+  match_concrete (RePreArrow f) = Just f
+  match_concrete _ = Nothing
+  match_id (PreArrowId) = IsId
+  match_id _ = NotId
+  match_compose (PreArrowCompo f g) = IsCompo f g
+  match_compose _ = NotCompo
+
+instance PreArrow k => CRCartesian (RePreArrow k) where
+  match_swap (PreArrowSwap) = IsSwap
+  match_swap _ = NotSwap
+  match_attachUnit (PreArrowAttachUnit) = IsAttachUnit
+  match_attachUnit _ = NotAttachUnit
+  match_detachUnit (PreArrowDetachUnit) = IsDetachUnit
+  match_detachUnit _ = NotDetachUnit
+  match_regroup (PreArrowRegroup) = IsRegroup
+  match_regroup _ = NotRegroup
+  match_regroup' (PreArrowRegroup_) = IsRegroup'
+  match_regroup' _ = NotRegroup'
+
+instance PreArrow k => CRMorphism (RePreArrow k) where
+  match_par (PreArrowPar f g) = IsPar f g
+  match_par _ = NotPar
+
+data FanPattern k α β where
+    IsFan :: (Object k α, ObjectPair k β γ)
+         => k α β -> k α γ -> FanPattern k α (β,γ)
+    NotFan :: FanPattern k α β
+data FstPattern k α β where
+    IsFst :: (ObjectPair k α β)
+                 => FstPattern k (α,β) α
+    NotFst :: FstPattern k α β
+data SndPattern k α β where
+    IsSnd :: (ObjectPair k α β)
+                 => SndPattern k (α,β) β
+    NotSnd :: SndPattern k α β
+data TerminalPattern k α β where
+    IsTerminal :: (Object k α, UnitObject k ~ u)
+                 => TerminalPattern k α u
+    NotTerminal :: TerminalPattern k α β
+class CRCartesian k => CRPreArrow k where
+  match_fan :: k α β -> FanPattern k α β
+  match_fst :: k α β -> FstPattern k α β
+  match_snd :: k α β -> SndPattern k α β
+  match_terminal :: k α β -> TerminalPattern k α β
+
+pattern f:&&&g <- (match_fan -> IsFan f g)
+pattern Fst <- (match_fst -> IsFst)
+pattern Snd <- (match_snd -> IsSnd)
+pattern Terminal <- (match_terminal -> IsTerminal)
+  
+instance PreArrow k => CRPreArrow (RePreArrow k) where
+  match_fan (PreArrowFanout f g) = IsFan f g
+  match_fan _ = NotFan
+  match_fst PreArrowFst = IsFst
+  match_fst _ = NotFst
+  match_snd PreArrowSnd = IsSnd
+  match_snd _ = NotSnd
+  match_terminal PreArrowTerminal = IsTerminal
+  match_terminal _ = NotTerminal
+
+
+instance WellPointed k => CRCategory (ReWellPointed k) where
+  type SpecificCat (ReWellPointed k) = k
+  fromSpecific = ReWellPointed
+  match_concrete (ReWellPointed f) = Just f
+  match_concrete _ = Nothing
+  match_id (WellPointedId) = IsId
+  match_id _ = NotId
+  match_compose (WellPointedCompo f g) = IsCompo f g
+  match_compose _ = NotCompo
+
+instance WellPointed k => CRCartesian (ReWellPointed k) where
+  match_swap (WellPointedSwap) = IsSwap
+  match_swap _ = NotSwap
+  match_attachUnit (WellPointedAttachUnit) = IsAttachUnit
+  match_attachUnit _ = NotAttachUnit
+  match_detachUnit (WellPointedDetachUnit) = IsDetachUnit
+  match_detachUnit _ = NotDetachUnit
+  match_regroup (WellPointedRegroup) = IsRegroup
+  match_regroup _ = NotRegroup
+  match_regroup' (WellPointedRegroup_) = IsRegroup'
+  match_regroup' _ = NotRegroup'
+
+instance WellPointed k => CRMorphism (ReWellPointed k) where
+  match_par (WellPointedPar f g) = IsPar f g
+  match_par _ = NotPar
+  
+instance WellPointed k => CRPreArrow (ReWellPointed k) where
+  match_fan (WellPointedFanout f g) = IsFan f g
+  match_fan _ = NotFan
+  match_fst WellPointedFst = IsFst
+  match_fst _ = NotFst
+  match_snd WellPointedSnd = IsSnd
+  match_snd _ = NotSnd
+  match_terminal WellPointedTerminal = IsTerminal
+  match_terminal _ = NotTerminal
+
+data ConstPattern k α β where
+    IsConst :: (Object k α, Object k β)
+                 => β -> ConstPattern k α β
+    NotConst :: ConstPattern k α β
+class CRPreArrow k => CRWellPointed k where
+  match_const :: k α β -> ConstPattern k α β
+
+pattern Const c <- (match_const -> IsConst c)
+  
+instance WellPointed k => CRWellPointed (ReWellPointed k) where
+  match_const (WellPointedConst c) = IsConst c
+  match_const _ = NotConst
+
+
+
